4c3fb031c14a331c1775fd382884b587cabb8d53
yueyuxuan-APP <1459987672@qq.com>
2020-05-10 00:46:11 +0800

    yyx   5.10  写了工作模块和新建日程

--- app/src/main/java/com/example/itile/Fragment/Guide/WorkFragment.java
+++ app/src/main/java/com/example/itile/Fragment/Guide/WorkFragment.java
@@ -2,30 +2,76 @@ package com.example.itile.Fragment.Guide;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CalendarView;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.itile.Adapter.WorkAdapter;
 import com.example.itile.AddressActivity;
 import com.example.itile.FormActivity;
+import com.example.itile.NewScheduleActivity;
 import com.example.itile.R;
 import com.example.itile.TaskActivity;
+import com.example.itile.Util.HttpUtil;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.Response;
 
 public class WorkFragment extends Fragment {
+    List<Map<String, Object>> list = new ArrayList<>();
 
+    private RecyclerView recyclerView;
     private ImageView imageView;
+    private String s = "hahahaha";
+    private String yyyy;
+    private String mm;
+    private String dd;
+    private String today;
+    private String post_time;
+    private ImageView jump_new_schedule;
+
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
 
 
         View root = inflater.inflate(R.layout.fragment_work, container, false);
-
-
+        Log.d("执行了oncreate", s);
+        //获取系统时间
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd HH-mm");
+        Date date = new Date(System.currentTimeMillis());
+        Log.d("当前时间",simpleDateFormat.format(date));
+        today = simpleDateFormat.format(date);
+        post_time = today;
+        yyyy = today.substring(0,4);
+        Log.d("yyyy",yyyy);
+        mm = today.substring(5,7);
+        Log.d("mm",mm);
+        dd = today.substring(8,10);
+        Log.d("dd",dd);
         return root;
 
     }
@@ -33,8 +79,18 @@ public class WorkFragment extends Fragment {
     @Override
     public void onResume() {
         super.onResume();
-
+        list.clear();
+        //新建日程
+        jump_new_schedule = getActivity().findViewById(R.id.work_new_schedule);
+        jump_new_schedule.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(), NewScheduleActivity.class);
+                startActivity(intent);
+            }
+        });
         imageView = getActivity().findViewById(R.id.tongxunlu);
+        recyclerView = getActivity().findViewById(R.id.recyclerView_work);
 
         imageView.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -42,6 +98,83 @@ public class WorkFragment extends Fragment {
                 Intent intent1 = new Intent();
                 intent1.setClass(getActivity(), AddressActivity.class); //从前者跳到后者，特别注意的是，在fragment中，用getActivity()来获取当前的activity
                 getActivity().startActivity(intent1);
+            }
+        });
+
+        //日历的点击操作
+        CalendarView calendarView = getActivity().findViewById(R.id.calendarview);
+        Log.d("getDate", String.valueOf(calendarView.getDate()));
+        calendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+
+            @Override
+            public void onSelectedDayChange(CalendarView arg0, int arg1,
+                                            int arg2, int arg3) {
+                list.clear();
+                yyyy = String.valueOf(arg1);
+                String temp1 = String.valueOf(arg2 + 1);
+                if(arg2 + 1 < 10)
+                    mm = "0" + temp1;
+                else
+                    mm  = temp1;
+                String temp2 = String.valueOf(arg3);
+                if(arg3 < 10)
+                    dd = "0" + temp2;
+                else
+                    dd = temp2;
+                post_time = yyyy + "-" + mm + "-" + dd + " 00-00";
+                Log.d("post_time",post_time);
+                postCalendar("http://118.190.245.170/worktile/calendar/",post_time);
+            }
+        });
+        postCalendar("http://118.190.245.170/worktile/calendar/",post_time);
+        Log.d("执行了onresume", s);
+    }
+
+    public void postCalendar(String address,String time) {
+        HttpUtil.postCalendar(address,time, new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                //得到服务器返回的具体内容
+                String responseData = response.body().string();
+                Log.d("postCalendar",responseData);
+                try {
+                    JSONObject jsonObject = new JSONObject(responseData);
+                    JSONArray jsonArray = jsonObject.getJSONArray("schedule");
+                    for (int i = 0; i < jsonArray.length(); i++) {
+                        JSONObject schedule = jsonArray.getJSONObject(i);
+                        String pk = schedule.getString("pk");
+                        JSONObject fields = schedule.getJSONObject("fields");
+                        String starttime = fields.getString("starttime");
+                        String endtime = fields.getString("endtime");
+                        String state = fields.getString("state");
+                        String description = fields.getString("description");
+
+                        Map map = new HashMap();
+                        map.put("pk", pk);
+                        map.put("starttime", starttime);
+                        map.put("endtime", endtime);
+                        map.put("state", state);
+                        map.put("description", description);
+
+                        list.add(map);
+                    }
+                    getActivity().runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));//垂直排列 , Ctrl+P
+                            recyclerView.setAdapter(new WorkAdapter(getActivity(), list));//绑定适配器
+                        }
+                    });
+                } catch (JSONException e) {
+
+                    e.printStackTrace();
+
+                }
             }
         });
     }
---
+++ app/src/main/java/com/example/itile/NewScheduleActivity.java
@@ -0,0 +1,368 @@
+package com.example.itile;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.itile.Adapter.WorkAdapter;
+import com.example.itile.Util.HttpUtil;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.Response;
+
+public class NewScheduleActivity extends AppCompatActivity {
+    private TextView sure;
+    private EditText editText;
+    private ImageView back;
+    private Spinner mspinner_nian;
+    private ArrayAdapter<String> adapter_nian;
+    private String yyyy="2020";
+    private String MM="05";
+    private String dd="09";
+    private String HH1="23";
+    private String mm1="29";
+    private String HH2="23";
+    private String mm2="59";
+    private String description;
+    private String starttime;
+    private String endtime;
+    private String warning;
+
+    private Spinner mspinner_yue;
+    private ArrayAdapter<String> adapter_yue;
+    private Spinner mspinner_ri;
+    private ArrayAdapter<String> adapter_ri;
+    private Spinner mspinner_shi1;
+    private ArrayAdapter<String> adapter_shi1;
+    private Spinner mspinner_fen1;
+    private ArrayAdapter<String> adapter_fen1;
+    private Spinner mspinner_shi2;
+    private ArrayAdapter<String> adapter_shi2;
+    private Spinner mspinner_fen2;
+    private ArrayAdapter<String> adapter_fen2;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.layout_new_schedule);
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.hide();
+        }
+        back = (ImageView)findViewById(R.id.back_from_new_schedule);
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+        editText = (EditText)findViewById(R.id.new_schedule_description);
+        sure = (TextView)findViewById(R.id.new_schedule_sure);
+        sure.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                description = editText.getText().toString();
+                if("".equals(description))
+                    Toast.makeText(NewScheduleActivity.this,"请您输入日程详情",Toast.LENGTH_SHORT).show();
+                else {
+                    postNewSchedule("http://118.190.245.170/worktile/newschedule/",starttime,endtime,description);
+                }
+            }
+        });
+        starttime = yyyy+"-"+MM+"-"+dd+" "+HH1+"-"+mm1;
+        endtime = yyyy+"-"+MM+"-"+dd+" "+HH2+"-"+mm2;
+
+        //选择年
+        String[] nian = new String[]{"年", "2018", "2019", "2020", "2021","2022","2023"};
+        //创建一个数组适配器
+        adapter_nian = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, nian);
+        adapter_nian.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);     //设置下拉列表框的下拉选项样式
+
+        mspinner_nian = findViewById(R.id.spinner_nian);
+        mspinner_nian.setAdapter(adapter_nian);
+        //点击事件
+        mspinner_nian.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+
+            private String positions;
+
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                positions = adapter_nian.getItem(position);
+                if (positions.equals("年")){
+                    //
+                }else{
+                    yyyy = positions;
+                    starttime = yyyy+"-"+MM+"-"+dd+" "+HH1+"-"+mm1;
+                    endtime = yyyy+"-"+MM+"-"+dd+" "+HH2+"-"+mm2;
+                    Toast.makeText(NewScheduleActivity.this,yyyy,Toast.LENGTH_SHORT).show();
+                }
+                parent.setVisibility(View.VISIBLE);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+                parent.setVisibility(View.VISIBLE);
+            }
+        });
+
+        //选择月
+        String[] yue = new String[]{"月", "01", "02", "03", "04","05","06","07","08","09","10","11","12"};
+        //创建一个数组适配器
+        adapter_yue = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, yue);
+        adapter_yue.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);     //设置下拉列表框的下拉选项样式
+
+        mspinner_yue = findViewById(R.id.spinner_yue);
+        mspinner_yue.setAdapter(adapter_yue);
+        //点击事件
+        mspinner_yue.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+
+            private String positions;
+
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                positions = adapter_yue.getItem(position);
+                if (positions.equals("月")){
+                    //
+                }else{
+                    MM = positions;
+                    starttime = yyyy+"-"+MM+"-"+dd+" "+HH1+"-"+mm1;
+                    endtime = yyyy+"-"+MM+"-"+dd+" "+HH2+"-"+mm2;
+                    Toast.makeText(NewScheduleActivity.this,MM,Toast.LENGTH_SHORT).show();
+                }
+                parent.setVisibility(View.VISIBLE);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+                parent.setVisibility(View.VISIBLE);
+            }
+        });
+
+        //选择日
+        String[] ri = new String[]{"日", "01", "02", "03", "04","05","06","07","08","09","10","11","12","13","14","15","16","17","18","19","20","21","22","23","24","25","26","27","28","29","30","31"};
+        //创建一个数组适配器
+        adapter_ri = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, ri);
+        adapter_ri.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);     //设置下拉列表框的下拉选项样式
+
+        mspinner_ri = findViewById(R.id.spinner_ri);
+        mspinner_ri.setAdapter(adapter_ri);
+        //点击事件
+        mspinner_ri.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+
+            private String positions;
+
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                positions = adapter_ri.getItem(position);
+                if (positions.equals("日")){
+                    //
+                }else{
+                    dd = positions;
+                    starttime = yyyy+"-"+MM+"-"+dd+" "+HH1+"-"+mm1;
+                    endtime = yyyy+"-"+MM+"-"+dd+" "+HH2+"-"+mm2;
+                    Toast.makeText(NewScheduleActivity.this,dd,Toast.LENGTH_SHORT).show();
+                }
+                parent.setVisibility(View.VISIBLE);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+                parent.setVisibility(View.VISIBLE);
+            }
+        });
+
+
+        //选择开始时
+        String[] shi1 = new String[]{"时", "01", "02", "03", "04","05","06","07","08","09","10","11","12","13","14","15","16","17","18","19","20","21","22","23","24"};
+        //创建一个数组适配器
+        adapter_shi1 = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, shi1);
+        adapter_shi1.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);     //设置下拉列表框的下拉选项样式
+
+        mspinner_shi1 = findViewById(R.id.spinner_HH1);
+        mspinner_shi1.setAdapter(adapter_shi1);
+        //点击事件
+        mspinner_shi1.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+
+            private String positions;
+
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                positions = adapter_shi1.getItem(position);
+                if (positions.equals("时")){
+                    //
+                }else{
+                    HH1 = positions;
+                    starttime = yyyy+"-"+MM+"-"+dd+" "+HH1+"-"+mm1;
+                    endtime = yyyy+"-"+MM+"-"+dd+" "+HH2+"-"+mm2;
+                    Toast.makeText(NewScheduleActivity.this,HH1,Toast.LENGTH_SHORT).show();
+                }
+                parent.setVisibility(View.VISIBLE);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+                parent.setVisibility(View.VISIBLE);
+            }
+        });
+
+        //选择开始分
+        String[] fen1 = new String[]{"分", "01", "02", "03", "04","05","06","07","08","09","10","11","12","13","14","15","16","17","18","19","20","21","22","23","24","25","26","27","28","29","30","31","32","33","34","35","36","37","38","39","40","41","42","43","44","45","46","47","48","49","50","51","52","53","54","55","56","57","58","59","60"};
+        //创建一个数组适配器
+        adapter_fen1 = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, fen1);
+        adapter_fen1.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);     //设置下拉列表框的下拉选项样式
+
+        mspinner_fen1 = findViewById(R.id.spinner_mm1);
+        mspinner_fen1.setAdapter(adapter_fen1);
+        //点击事件
+        mspinner_fen1.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+
+            private String positions;
+
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                positions = adapter_fen1.getItem(position);
+                if (positions.equals("分")){
+                    //
+                }else{
+                    mm1 = positions;
+                    starttime = yyyy+"-"+MM+"-"+dd+" "+HH1+"-"+mm1;
+                    endtime = yyyy+"-"+MM+"-"+dd+" "+HH2+"-"+mm2;
+                    Toast.makeText(NewScheduleActivity.this,mm1,Toast.LENGTH_SHORT).show();
+                }
+                parent.setVisibility(View.VISIBLE);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+                parent.setVisibility(View.VISIBLE);
+            }
+        });
+
+        //选择结束时
+        String[] shi2 = new String[]{"时", "01", "02", "03", "04","05","06","07","08","09","10","11","12","13","14","15","16","17","18","19","20","21","22","23","24"};
+        //创建一个数组适配器
+        adapter_shi2 = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, shi2);
+        adapter_shi2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);     //设置下拉列表框的下拉选项样式
+
+        mspinner_shi2 = findViewById(R.id.spinner_HH2);
+        mspinner_shi2.setAdapter(adapter_shi2);
+        //点击事件
+        mspinner_shi2.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+
+            private String positions;
+
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                positions = adapter_shi2.getItem(position);
+                if (positions.equals("时")){
+                    //
+                }else{
+                    HH2 = positions;
+                    starttime = yyyy+"-"+MM+"-"+dd+" "+HH1+"-"+mm1;
+                    endtime = yyyy+"-"+MM+"-"+dd+" "+HH2+"-"+mm2;
+                    Toast.makeText(NewScheduleActivity.this,HH2,Toast.LENGTH_SHORT).show();
+                }
+                parent.setVisibility(View.VISIBLE);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+                parent.setVisibility(View.VISIBLE);
+            }
+        });
+
+        //选择开始分
+        String[] fen2 = new String[]{"分", "01", "02", "03", "04","05","06","07","08","09","10","11","12","13","14","15","16","17","18","19","20","21","22","23","24","25","26","27","28","29","30","31","32","33","34","35","36","37","38","39","40","41","42","43","44","45","46","47","48","49","50","51","52","53","54","55","56","57","58","59","60"};
+        //创建一个数组适配器
+        adapter_fen2 = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, fen2);
+        adapter_fen2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);     //设置下拉列表框的下拉选项样式
+
+        mspinner_fen2 = findViewById(R.id.spinner_mm2);
+        mspinner_fen2.setAdapter(adapter_fen2);
+        //点击事件
+        mspinner_fen2.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+
+            private String positions;
+
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                positions = adapter_fen2.getItem(position);
+                if (positions.equals("分")){
+                    //
+                }else{
+                    mm2 = positions;
+                    starttime = yyyy+"-"+MM+"-"+dd+" "+HH1+"-"+mm1;
+                    endtime = yyyy+"-"+MM+"-"+dd+" "+HH2+"-"+mm2;
+                    Toast.makeText(NewScheduleActivity.this,mm2,Toast.LENGTH_SHORT).show();
+                }
+                parent.setVisibility(View.VISIBLE);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+                parent.setVisibility(View.VISIBLE);
+            }
+        });
+    }
+
+    public void postNewSchedule(String address,String starttime,String endtime,String description) {
+        HttpUtil.postNewSchedule(address,starttime,endtime,description, new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                //得到服务器返回的具体内容
+                String responseData = response.body().string();
+                Log.d("postNewCalendar",responseData);
+                try {
+                    JSONObject jsonObject = new JSONObject(responseData);
+                    warning = jsonObject.getString("warning");
+
+                   runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            if(warning.equals("1")){
+                                Toast.makeText(NewScheduleActivity.this,"新建日程成功",Toast.LENGTH_SHORT).show();
+                                finish();
+                            }else {
+                                Toast.makeText(NewScheduleActivity.this,warning,Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    });
+                } catch (JSONException e) {
+
+                    e.printStackTrace();
+
+                }
+            }
+        });
+    }
+
+}
